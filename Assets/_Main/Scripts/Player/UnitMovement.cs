7f294a2122c804d9eee971ea10591aba3d80c95d
JuanBain <juansbain@gmail.com>
2022-02-25 13:08:17 -0500

    Import mirror,TMP,Progrids,Probuilder,cars

---
+++ Assets/_Main/Scripts/Player/UnitMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Mirror;
+
+
+public class UnitMovement : NetworkBehaviour
+{
+    private NavMeshAgent agent = null;
+    private Camera maincamera;
+
+    #region Server
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    [Command]
+    private void CmdMove(Vector3 position)
+    {
+        if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
+        agent.SetDestination(hit.position);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        maincamera = Camera.main;
+    }
+
+    [ClientCallback]
+
+    private void Update()
+    {
+        if (!hasAuthority) { return; }
+
+        if (!Input.GetMouseButtonDown(1)) { return; }
+
+        Ray ray = maincamera.ScreenPointToRay(Input.mousePosition);
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
+
+        CmdMove(hit.point);
+    }
+
+    #endregion
+}
