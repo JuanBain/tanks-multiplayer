f4895c81ff7a499c6de1e30c0a4e73dcace595fa
JuanBain <juansbain@gmail.com>
2022-03-15 11:24:00 -0500

    Dealing damage and Health UI

--- Assets/_Main/Scripts/Combat/Health.cs
+++ Assets/_Main/Scripts/Combat/Health.cs
@@ -1,18 +1,48 @@
+using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int maxHealth = 100;
+
+    [SyncVar(hook = nameof(HandleHealthUpdated))]
+    private int currentHealth;
+
+    public event Action ServerOnDie;
+
+    public event Action<int, int> ClientOnHealthUpdated;
+
+    #region server
+
+    public override void OnStartServer()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    [Server]
+    public void DealDamage(int damageAmount)
     {
-        
+        if (currentHealth == 0) { return; }
+
+        currentHealth = Mathf.Max(currentHealth - damageAmount, 0);
+
+        if (currentHealth != 0) { return; }
+
+        ServerOnDie?.Invoke();
+
+        Debug.Log("We Died");
     }
+    #endregion
+
+
+    #region Client
+
+    private void HandleHealthUpdated(int oldHealth, int newHealth)
+    {
+        ClientOnHealthUpdated?.Invoke(newHealth, maxHealth);
+    }
+    #endregion
 }
--- Assets/_Main/Scripts/Units/UnitProjectile.cs
+++ Assets/_Main/Scripts/Units/UnitProjectile.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
 
@@ -19,7 +20,24 @@ public class UnitProjectile : NetworkBehaviour
         Invoke(nameof(DestroySelf), destroyAfterSeconds);
     }
 
-    [Server] 
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) { return; }
+        }
+
+        if (other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damageToDeal);
+        }
+
+        DestroySelf();
+    }
+
+
+    [Server]
     private void DestroySelf()
     {
         NetworkServer.Destroy(gameObject);
